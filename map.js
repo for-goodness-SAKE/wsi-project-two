ebbe4574659441a4e3e9d95b697fc8a3375a7b63
Emma Prager <46731977+eprager@users.noreply.github.com>
2021-03-10 16:32:55 -0500

    Map: Modifying icon colors, removing some placeholder locations (#17)
    * Placeholder Google Maps JavaScript API
    
    For results to be displayed
    
    * Modifying icon colors, removing some placeholder locations
    
    The icon for the user will be a blue pin with a star, the icon for the parks will be an empty pink pin.  The placeholder locations in the lake were removed.

--- map.js
+++ map.js
@@ -4,27 +4,26 @@ function initMap() {
     center: iit,
     zoom: 8,
   });
+  const iconBase = "http://maps.google.com/mapfiles/kml/paddle/";
   const marker = new google.maps.Marker({
     position: iit,
+    icon: iconBase + "blu-stars.png",
     map: map,
   });
-
   const markers = locations.map((location, i) => {
     return new google.maps.Marker({
       position: location,
+      icon: iconBase + "pink-blank.png",
       map: map,
     });
   });
 }
 
 const locations = [
-  { lat: 41.834819, lng: -87.626893 },
   { lat: 41.80, lng: -87.77 },
   { lat: 41.82, lng: -87.61 },
   { lat: 40.9, lng: -87.6 },
   { lat: 41.65, lng: -87.5 },
-  { lat: 41.80, lng: -87.4 },
-  { lat: 41.85, lng: -87.3 },
   { lat: 41.84, lng: -87.8 },
   { lat: 41.99, lng: -87.9 },
   { lat: 41.89, lng: -88 },
