bf5f1b9da9833510779faa5e1d299594e8026234
Sneha Rajulapally <63931347+SnehaRajulapally@users.noreply.github.com>
2021-03-11 10:57:31 -0500

    Update files with styling and checkbox dropdown options (#18)
    * Update files with styling and checkbox dropdown options
    
    * Update README.md
    
    * Update README.md
    
    * Rename app.json to app.js

--- app.js
+++ app.js
@@ -9,7 +9,3 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}`)
 })
-<<<<<<< HEAD
-=======
-
->>>>>>> 784837308ee68f19c2d16df7f6c6fb57ceb3be9f
--- search.js
+++ search.js
@@ -1,35 +1,47 @@
 function search(){
-    if(document.getElementById("loc").value.length == 0) {
-        getLocation();
-    } else {
-        var geocoder = new google.maps.Geocoder();
-        var address= document.getElementById("loc").value;
-        geocoder.geocode( { 'address': address}, function(results, status) {
-            if (status == google.maps.GeocoderStatus.OK) {
-                var lat = results[0].geometry.location.lat();
-                var long= results[0].geometry.location.lng();
-                var location = lat + ", "+ long;
-                console.log(location);
-            }
-        });
-    }
-    function getLocation() {
-        if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(showPosition);
-        }
-    }
-    function showPosition(position) {
-        var lat =  position.coords.latitude
-        var long = position.coords.longitude;
-        var location = lat + ", "+ long;
-    }
-    var radius=document.getElementById("rad").value;
+  if(document.getElementById("loc").value.length == 0) {
+      getLocation();
+  } else {
+      var geocoder = new google.maps.Geocoder();
+      var address= document.getElementById("loc").value;
+      geocoder.geocode( { 'address': address}, function(results, status) {
+          if (status == google.maps.GeocoderStatus.OK) {
+              var lat = results[0].geometry.location.lat();
+              var long= results[0].geometry.location.lng();
+              var location = lat + ", "+ long;
+              console.log(location);
+          }
+      });
+  }
+  function getLocation() {
+      if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(showPosition);
+      }
+  }
+  function showPosition(position) {
+      var lat =  position.coords.latitude
+      var long = position.coords.longitude;
+      var location = lat + ", "+ long;
+  }
+  var radius=document.getElementById("rad").value;
+  console.log(radius);
 }
 
 var expanded = false;
 
 function showCheckboxes1() {
-  var checkboxes = document.getElementById("checkboxes1");
+var checkboxes = document.getElementById("checkboxes1");
+if (!expanded) {
+  checkboxes.style.display = "block";
+  expanded = true;
+} else {
+  checkboxes.style.display = "none";
+  expanded = false;
+}
+}
+
+function showCheckboxes2() {
+  var checkboxes = document.getElementById("checkboxes2");
   if (!expanded) {
     checkboxes.style.display = "block";
     expanded = true;
@@ -39,17 +51,6 @@ function showCheckboxes1() {
   }
 }
 
-function showCheckboxes2() {
-    var checkboxes = document.getElementById("checkboxes2");
-    if (!expanded) {
-      checkboxes.style.display = "block";
-      expanded = true;
-    } else {
-      checkboxes.style.display = "none";
-      expanded = false;
-    }
-  }
-
 function reset() {
-    document.getElementById("filter").reset();
+  document.getElementById("filter").reset();
 }
